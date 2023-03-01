4c26d0245d765a03dbabb7fca8b3b23c2448ca58
Bryan Kenote <bryan.kenote@faithlife.com>
2023-02-28 18:50:00 -0800

    improve visuals

--- config.lua
+++ config.lua
@@ -12,6 +12,68 @@ an executable
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = true
 lvim.colorscheme = "vscode"
+lvim.builtin.lualine.options.theme = 'vscode'
+lvim.builtin.lualine.style = "default"
+
+lvim.builtin.bufferline.options = {
+  always_show_bufferline = false,
+  buffer_close_icon = "",
+  close_command = "Bdelete %d",
+  close_icon = "",
+  indicator = {
+    style = "icon",
+    icon = " ",
+  },
+  left_trunc_marker = "",
+  modified_icon = "●",
+  offsets = { { filetype = "NvimTree", text_align = "center" } },
+  right_mouse_command = "Bdelete! %d",
+  right_trunc_marker = "",
+  show_close_icon = false,
+  show_tab_indicators = true,
+}
+lvim.builtin.bufferline.highlights = {
+  fill = {
+    fg = { attribute = "fg", highlight = "Normal" },
+    bg = { attribute = "bg", highlight = "StatusLineNC" },
+  },
+  background = {
+    fg = { attribute = "fg", highlight = "Normal" },
+    bg = { attribute = "bg", highlight = "StatusLine" },
+  },
+  buffer_visible = {
+    fg = { attribute = "fg", highlight = "Normal" },
+    bg = { attribute = "bg", highlight = "Normal" },
+  },
+  buffer_selected = {
+    fg = { attribute = "fg", highlight = "Normal" },
+    bg = { attribute = "bg", highlight = "Normal" },
+  },
+  separator = {
+    fg = { attribute = "bg", highlight = "Normal" },
+    bg = { attribute = "bg", highlight = "StatusLine" },
+  },
+  separator_selected = {
+    fg = { attribute = "fg", highlight = "Special" },
+    bg = { attribute = "bg", highlight = "Normal" },
+  },
+  separator_visible = {
+    fg = { attribute = "fg", highlight = "Normal" },
+    bg = { attribute = "bg", highlight = "StatusLineNC" },
+  },
+  close_button = {
+    fg = { attribute = "fg", highlight = "Normal" },
+    bg = { attribute = "bg", highlight = "StatusLine" },
+  },
+  close_button_selected = {
+    fg = { attribute = "fg", highlight = "Normal" },
+    bg = { attribute = "bg", highlight = "Normal" },
+  },
+  close_button_visible = {
+    fg = { attribute = "fg", highlight = "Normal" },
+    bg = { attribute = "bg", highlight = "Normal" },
+  },
+}
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
