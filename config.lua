f09864043add8f7d1a32d56f269e8ddc08eee935
Bryan Kenote <bryan.kenote@faithlife.com>
2023-03-01 16:17:22 -0800

    add lazygit plugin

--- config.lua
+++ config.lua
@@ -138,6 +138,10 @@ lvim.builtin.which_key.mappings["t"] = {
   r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
 }
 
+lvim.builtin.which_key.mappings["gl"] = {
+  ":LazyGit<CR>", "Lazy Git"
+}
+
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
@@ -439,6 +443,7 @@ lvim.plugins = {
       }, { prefix = "<leader>" })
     end
   },
+  { "kdheepak/lazygit.nvim" },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
